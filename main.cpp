50fecd03f80b0cd0acfc07b0d517ec51c9c19d25
KianaJaberi <152029042+KianaJaberi@users.noreply.github.com>
2024-07-12 18:09:19 +0330

    main with Player class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <unordered_map>
 
 using namespace std ;
@@ -43,8 +44,91 @@ class State {
 
 class Player {
     public :
+        void setName ( string n ){
+            name = n ;
+        }
+
+        string getName (){
+            return name ;
+        }
+
+        void setColor ( string c ){
+            color = c ;
+        }
+
+        string getColor (){
+            return color ;
+        }
+
+        void setStates ( State s ){
+            states.push_back (s) ;
+        }
+
+        int numberOfStates (){
+            return states.size () ;
+        }
+
+        bool checkStates (){
+            if ( states.size () == 5 ){
+                return true ;
+            }
+            else {
+                int counter = 0 ;
+
+                for ( int i = 0 ; i < states.size () ; i ++ ){
+                    for ( int j = 0 ; j < states.size () ; j ++ ){
+                        if ( states [i].getVicinityState ( states [j].getName () ) == true ){
+                            counter ++ ;
+                        }
+                    }
+
+                    if ( counter == 2 ){
+                        return true ;
+                    }
+                    else {
+                        counter = 0 ;
+                    }
+                }
+                return false ;
+            }
+        }
+
+        void setCards ( vector < Card >& temp ){
+            for ( int i = 0 ; i < temp.size () ; i ++ ){
+                cards.push_back ( temp [i] ) ;
+            }
+        }
+
+        void getCards ( vector < Card >& temp ){
+            for ( int i = 0 ; i < cards.size () ; i ++ ){
+                temp [i] = cards [i] ;
+            }
+        }
+
+        bool checkCards (){
+            if ( cards.size () == 0 ){
+                return true ;
+            }
+            else {
+                return false ;
+            }
+        }
+
+        void deleteCard ( Card c ){
+            for ( int i = 0 ; i < cards.size () ; i ++ ){
+                if ( cards [i].getName () == c.getName () ){
+                    auto it = cards.begin () + i ;
+                    cards.erase ( it ) ;
+                    break ;
+                }
+            }
+        }
 
     private :
+        string name ;
+        string color ;
+        vector < State > states ;
+        vector < Card > cards ;
 };
 
 class Card {
