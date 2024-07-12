6b7c079009d70ee1045a0c1a83f340a1ea0774ad
KianaJaberi <152029042+KianaJaberi@users.noreply.github.com>
2024-07-12 14:44:09 +0330

    main file

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std ;
+
+class State {
+    public :
+
+    private :
+};
+
+class Player {
+    public :
+
+    private :
+};
+
+class Card {
+    public :
+
+    private :
+};
+
+class YellowCards : public Card {
+    public :
+
+    private :
+};
+
+class PurpleCards : public Card {
+    public :
+
+    private :
+};
+
+class Condottiere {
+    public :
+
+    private :
+};
+
+int main (){
+
+    return 0 ;
+}
